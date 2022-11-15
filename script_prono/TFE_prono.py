1bf2f63597656a859201f2c9047fa97132dcb3dd
nsutter <nsutter@etu.unistra.fr>
2022-11-15 19:49:00 +0100

    Update TFE script to take the good arguments

--- script_prono/TFE_prono.py
+++ script_prono/TFE_prono.py
@@ -1,30 +1,30 @@
 import sys
 import random
 
-country_list = [["Allemagne", 11] , 
-                ["Angleterre", 5] , 
-                ["Arabie_saoudite", 51] , 
+country_list = [["Allemagne", 11] ,
+                ["Angleterre", 5] ,
+                ["Arabie_saoudite", 51] ,
                 ["Argentine", 3] ,
-                ["Australie", 38] , 
-                ["Belgique", 2] , 
-                ["Bresil", 1] , 
-                ["Cameroun", 43] , 
-                ["Canada", 41] , 
+                ["Australie", 38] ,
+                ["Belgique", 2] ,
+                ["Bresil", 1] ,
+                ["Cameroun", 43] ,
+                ["Canada", 41] ,
                 ["Croatie", 12] ,
                 ["Coree_du_sud", 28] ,
-                ["Costa_rica", 31] , 
-                ["Danemark", 10] , 
+                ["Costa_rica", 31] ,
+                ["Danemark", 10] ,
                 ["Equateur", 44] ,
                 ["Espagne", 7] ,
                 ["France", 4] ,
-                ["Ghana", 61] , 
+                ["Ghana", 61] ,
                 ["Iran", 20] ,
-                ["Japon", 24] , 
+                ["Japon", 24] ,
                 ["Maroc", 22] ,
                 ["Mexique", 13] ,
                 ["Pologne", 26] ,
                 ["Pays_bas", 8] ,
-                ["Pays_de_galles", 19] , 
+                ["Pays_de_galles", 19] ,
                 ["Portugal", 9] ,
                 ["Qatar", 50] ,
                 ["Senegal", 18] ,
@@ -61,9 +61,8 @@ def get_score(team1, team2):
     return score_team1, score_team2
 
 def main():
-    score1, score2 = get_score(sys.argv[1], sys.argv[3])
+    score1, score2 = get_score(sys.argv[1], sys.argv[2])
     print( str(score1) + " - " + str(score2) )
 
 if __name__ == "__main__":
     main()
-
